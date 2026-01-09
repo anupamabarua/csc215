acb0da4497c7dc908705e400cef0a5eee439c324
Anupama Barua <anupamabaruaa@gmail.com>
2026-01-09 18:58:56 -0500

    everything finally works and passes

--- BigInt/BIGINTTD.C
+++ BigInt/BIGINTTD.C
@@ -1,33 +1,36 @@
 #include "BIGINT.H"
 #include "BDSCTEST.H"
 
-main() {
+main()
+{
     struct bigint bi;
-    struct bigint bi1, bi2, result;
-    
+    struct bigint bi1;
+    struct bigint bi2;
+    struct bigint result;
+
     START_TESTING("BIGINTTD.C");
 
-    TEST_CASE("Read and write bigint 1234567") {
-        struct bigint bi;
+    TEST_CASE("Read and write bigint 1234567");
         set_bigint("1234567", &bi);
-        ASSERT_STR(get_bigint(bi), "1234567");
-    } 
+        ASSERT_STR(get_bigint(&bi), "1234567");
 
-    TEST_CASE("add single digit 1+5") {
-        struct bigint bi1, bi2, result;
+    TEST_CASE("add single digit 1+5");
         set_bigint("1", &bi1);
         set_bigint("5", &bi2);
         add_bigints(&bi1, &bi2, &result);
         ASSERT_STR(get_bigint(&result), "6");
-    }
 
-    TEST_CASE("Add double digit: 25 + 46") {
-            struct bigint bi1, bi2, result;
-            set_bigint("25", &bi1);
-            set_bigint("46", &bi2);
-            add_bigints(&bi1, &bi2, &result);
-            ASSERT_STR(get_bigint(result), "71");
-    }
+    TEST_CASE("add double digit 10+18");
+        set_bigint("10", &bi1);
+        set_bigint("18", &bi2);
+        add_bigints(&bi1, &bi2, &result);
+        ASSERT_STR(get_bigint(&result), "28");
+
+    TEST_CASE("add ten digit 3333333333+3333333333");
+        set_bigint("3333333333", &bi1);
+        set_bigint("3333333333", &bi2);
+        add_bigints(&bi1, &bi2, &result);
+        ASSERT_STR(get_bigint(&result), "6666666666");
 
     END_TESTING();
 }
