5e3954a48979553ebf53714fe43c963c27a4a71b
Anupama Barua <anupamabaruaa@gmail.com>
2026-01-09 18:09:54 -0500

    more test and fixing t hem

--- BigInt/BIGINTTD.C
+++ BigInt/BIGINTTD.C
@@ -10,12 +10,28 @@ main() {
         ASSERT_STR(get_bigint(bi), "1234567");
     } 
 
-    TEST_CASE("add big int 1+5") {
+    TEST_CASE("add single digit 1+5") {
         struct bigint bi1, bi2, result;
         set_bigint("1", &bi1);
         set_bigint("5", &bi2);
         add_bigints(&bi1, &bi2, &result);
         ASSERT_STR(get_bigint(&result), "6");
+    }
+
+        TEST_CASE("add double digit 10+18") {
+        struct bigint bi1, bi2, result;
+        set_bigint("10", &bi1);
+        set_bigint("18", &bi2);
+        add_bigints(&bi1, &bi2, &result);
+        ASSERT_STR(get_bigint(&result), "21");
+    }
+
+        TEST_CASE("add ten digit 4738291056+9182647305") {
+        struct bigint bi1, bi2, result;
+        set_bigint("10", &bi1);
+        set_bigint("18", &bi2);
+        add_bigints(&bi1, &bi2, &result);
+        ASSERT_STR(get_bigint(&result), "13920938361");
     }
 
     END_TESTING();
