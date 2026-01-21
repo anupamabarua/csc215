70c81b3c2522a89c289172ba6ac35bff239028a7
Anupama Barua <anupamabaruaa@gmail.com>
2026-01-20 21:14:20 -0500

    insert before node added

--- link/LINKLIST.C
+++ link/LINKLIST.C
@@ -9,3 +9,38 @@ struct node *mknode(n) int n; {
     x->next = NULL;
     return x;
 }
+struct node* insertBeforeNode(head, newval, beforeval)
+struct node* head;
+int newval;
+int beforeval;
+{
+    struct node* x;
+    struct node* prev;
+    struct node* cur;
+
+    if (head == NULL) {
+        return mknode(newval);
+    }
+
+    if (head->num == beforeval) {
+        x = mknode(newval);
+        x->next = head;
+        return x;
+    }
+
+    prev = head;
+    cur  = head->next;
+
+    while (cur != NULL) {
+        if (cur->num == beforeval) {
+            x = mknode(newval);
+            x->next = cur;
+            prev->next = x;
+            return head;
+        }
+        prev = cur;
+        cur = cur->next;
+    }
+    
+    return head;
+}
--- link/TESTLL.C
+++ link/TESTLL.C
@@ -12,5 +12,16 @@ main() {
       ASSERT(mynode->next == NULL);
   }
 
+  TEST_CASE("insertBeforeNode") {
+    struct node* l4;
+    l4 = mknode(10);
+
+    l4 = insertBeforeNode(l4, 0, 10);
+    ASSERT(l4->num == 0);
+
+    l4 = insertBeforeNode(l4, 5, 10);
+    ASSERT(l4->next->num == 5);
+  }
+
   END_TESTING();
 }
